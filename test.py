747f85f20e5389c384159f10f612c06342b0510a
Kendra <kendra@Zipcoders-MacBook-Pro-45.local>
2022-08-25 13:39:33 -0400

    cleaned up files and project structure

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
-import plotly.graph_objects as go
+
 import pandas as pd
-import numpy as np
-from datetime import datetime as dt
-import plotly.express as px
+
 
 car_sales = pd.read_csv('car_sales_clean.csv')
 df = pd.read_csv('ff_cars_clean.csv')
@@ -10,34 +8,10 @@ df = pd.read_csv('ff_cars_clean.csv')
 input_movie =input('> ')
 
 def update_statistics(input_movie):
-    #df_update = df[(df['Film Order'].str.contains(input_movie))]
-   #maj = df_update[(df_update['Role'].str.contains('Major'))].value_counts()
-   # maj = df_update.Role.str.contains('Major').sum()
-   #  avg_car_sale1 = round(car_sales['Sale Amount'].mean(), ndigits=0)
-   #  avg_car_sale2 = round(df['mean'].mean(), ndigits=0)
-    #print(df[df['Make'] == 'Corvette Sting Ray']['max'].item())
+
 
 
     print(len(df['Car Name'].unique()))
 
-
-
-    # cars = df[df["Car Name"] == ['Nissan Skyline GT-R R33', 'Corvette Sting Ray']]['Model']
-    # print(cars)
-    #
-    # df_update = car_sales[(car_sales['Model'].isin(cars))]
-    # print(df_update)
-    #
-    # df_update = df_update.sort_values(["Sale Date"]).reset_index(drop=True)
-    # print(df_update)
-
-    #print((f"{np.array2string(max_sale_date, formatter={'date': lambda x: f'{x:}'}, separator=', ').strip('[]')}"))
-
-    #(f"{np.array2string(max_car, formatter={'float': lambda x: f'{x:,}'}, separator=', ').strip('[]')}")
-    #print(df.query('Make' == 'Corvette Sting Ray')['max'])
-
-    #print(len(df_update))
-
-    #
 
 update_statistics(input_movie)
